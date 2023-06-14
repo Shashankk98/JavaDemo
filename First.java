8cb387d4302bb13471dc113c2feaa9e32a491502
Shashank Bukshetwar <shashank.b8909@gmail.com>
2023-06-15 00:41:49 +0530

    First commit

---
+++ First.java
@@ -0,0 +1,15 @@
+public class ifElseLoop {
+
+    public static void main(String[] args) {
+
+        int n= 80;
+
+        if(n>=0 && n<=100){
+            System.out.println("The Number is Between 0-100");
+        }
+        else {
+            System.out.println("The Number is greater than 100 ");
+        }
+        System.out.println();
+    }
+}
---
+++ Second.java
@@ -0,0 +1,12 @@
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+        int a=40;
+        int b=50;
+        int c=30;
+
+        String abc=(a<b)? "a is grater" : "B is greater";
+        System.out.println(abc);
+
+    }
+}
